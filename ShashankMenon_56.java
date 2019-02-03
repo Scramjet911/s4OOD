50ce9395ba8efe1c4725b9edb2f98699d167a99c
Scramjet911 <36035352+Scramjet911@users.noreply.github.com>
2019-02-03 23:32:14 +0530

    Update ShashankMenon_56.java

--- ShashankMenon_56.java
+++ ShashankMenon_56.java
@@ -4,25 +4,28 @@ import java.util.*;
 
 public class ShashankMenon_56 {
     public static void main(String[] args) {
-        int n;
+        int n1,n2;
         System.out.println("Enter The Number of Rows of the Diamond : ");
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
+        n1 = sc.nextInt();
+        System.out.println("Enter The Thickness of the Boundary (in number of 1's) : ");
+        n2 = sc.nextInt();
+        
         int i = 0, j = 0, k = 0;
-        for(i = 0;i<n;i++){
-            if(2*i>=n-1)
-                k=n-i-1;
+        for(i = 0;i<n1;i++){
+            if(2*i>=n1-1)
+                k=n1-i-1;
             else
                 k=i;
-            for(j = k;j<n;j++){
+            for(j = k;j<n1;j++){
                 System.out.print(" ");
             }
-            if(i==0||i==n){
+            if(i==0||i==n1){
                 System.out.println("1");
                 continue;
             }
             for(j = 0;j<=2*k;j++){
-                if((j<11)||(j > (2*k-11)))
+                if((j<n2)||(j > (2*k-n2)))
                     System.out.print("1");
                 else
                     System.out.print(" ");
